df65e44c12e8ace6319b0a82e7e84afced07b825
Gaming Unlimited <GamingsUnlimited@gmail.com>
2016-12-23 01:08:18 -0500

    Finished first team selection panel and editted faction pictures

--- gamemode/config/cl_fonts.lua
+++ gamemode/config/cl_fonts.lua
@@ -5,3 +5,19 @@ surface.CreateFont("FalloutRP1", {
 	shadow = true,
 	font = "monofonto"
 })
+
+surface.CreateFont("FalloutRP2", {
+	size = 18,
+	weight = 600,
+	antialias = true,
+	shadow = true,
+	font = "monofonto"
+})
+
+surface.CreateFont("FalloutRP3", {
+	size = 24,
+	weight = 600,
+	antialias = true,
+	shadow = true,
+	font = "monofonto"
+})
