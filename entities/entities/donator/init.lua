039b3e90e3d4291570eaf4823db1b2d7da6e39f5
Gaming Unlimited <GamingsUnlimited@gmail.com>
2017-02-07 09:33:23 -0500

    Added token shop with selection screen

---
+++ entities/entities/donator/init.lua
@@ -0,0 +1,24 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include("shared.lua")
+
+function ENT:Initialize()
+	self:SetModel("models/humans/group01/female_04.mdl")
+	self:SetHullType(HULL_HUMAN)
+	
+	self:SetNPCState(NPC_STATE_SCRIPT)
+	self:SetSolid(SOLID_BBOX)
+	self:CapabilitiesAdd(CAP_ANIMATEDFACE, CAP_TURN_HEAD)
+	self:SetUseType(SIMPLE_USE)
+ 
+	self:SetMaxYawSpeed(90)
+end
+
+function ENT:AcceptInput(type, activator)
+	if type == "Use" and IsValid(activator) and activator:IsPlayer() then
+		activator:openTokenShop()
+	end
+end
+
+ 
