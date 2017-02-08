2c1c443ad02f75f3e3f3171821e3f7bc6d8700f8
NickSchleicher <nschle3@students.towson.edu>
2017-02-07 19:32:29 -0500

    Move team enumerations to shared.lua so it's available to all files on startup

--- gamemode/modules/core/sh_teams.lua
+++ gamemode/modules/core/sh_teams.lua
@@ -1,9 +1,4 @@
 
-TEAM_BOS = 1
-TEAM_NCR = 2
-TEAM_LEGION = 3
-TEAM_FACTIONLESS = 4
-
 team.SetUp(TEAM_BOS, "Brotherhood of Steel", Color(255, 0, 0, 255))
 team.SetUp(TEAM_NCR, "New California Republic", Color(0, 255, 0, 255))
 team.SetUp(TEAM_LEGION, "Legion", Color(0, 0, 255, 255))
