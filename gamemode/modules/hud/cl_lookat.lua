e93ee9fdd4a15ad9e164f8548f1ea67b9a61ec1c
NickSchleicher <nschle3@students.towson.edu>
2017-02-09 14:45:57 -0500

    Draw player rank in a smaller font

--- gamemode/modules/hud/cl_lookat.lua
+++ gamemode/modules/hud/cl_lookat.lua
@@ -25,8 +25,8 @@ local function drawPlayerInfo()
 				draw.DrawText(ply:getName(), "FalloutRP4", pos.x + 1, pos.y - 79, colors.black, 1)
 				draw.DrawText(ply:getName(), "FalloutRP4", pos.x, pos.y - 80, Color(255, 255, 100, 255), 1)							
 				
-				draw.DrawText(ply:getRank(), "FalloutRP4", pos.x + 1, pos.y - 49, colors.black, 1)
-				draw.DrawText(ply:getRank(), "FalloutRP4", pos.x, pos.y - 50, Color(255, 255, 100, 255), 1)				
+				draw.DrawText(ply:getRank(), "FalloutRP3", pos.x + 1, pos.y - 49, colors.black, 1)
+				draw.DrawText(ply:getRank(), "FalloutRP3", pos.x, pos.y - 50, Color(255, 255, 100, 255), 1)				
 				
 				draw.DrawText("Level: " ..ply:getLevel(), "FalloutRP3", pos.x + 1, pos.y - 19, colors.black, 1)
 				draw.DrawText("Level: " ..ply:getLevel(), "FalloutRP3", pos.x, pos.y - 20, Color(255, 255, 100, 255), 1)
