8868ac2c460a98355316e9971e708031ff10e6ae
NickSchleicher <nschle3@students.towson.edu>
2017-02-13 14:49:10 -0500

    Added /dropmoney and stopped drawing models for apparel items because we don't have models for them yet so they're errors.

--- gamemode/modules/currency/sv_currency.lua
+++ gamemode/modules/currency/sv_currency.lua
@@ -12,3 +12,19 @@ function meta:addCaps(amount)
 	
 	MySQLite.query("UPDATE playerdata SET bottlecaps = " ..self:getCaps() .." WHERE steamid = '" ..self:SteamID() .."'")
 end
+
+function dropCaps(ply, text)
+	local split = string.Split(text, " ")
+	if (#split == 2) and (split[1] == "/dropmoney") and (tonumber(split[2])) and (tonumber(split[2]) > 0) then
+		local amount = tonumber(split[2])
+		if ply:canAfford(amount) then
+			ply:addCaps(-amount)
+			createLoot(ply:GetPos() + (ply:GetForward() * 25), {createItem(0, amount)})
+			
+			return ""
+		else
+			ply:notify("You do not have that many caps.", NOTIFY_ERROR)
+		end
+	end
+end
+hook.Add("PlayerSay", "dropCaps", dropCaps)
