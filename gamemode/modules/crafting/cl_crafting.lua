2d480ace33f511b11caee72bde8691e23fc3ac65
NickSchleicher <nschle3@students.towson.edu>
2017-06-13 23:58:56 -0400

    Remember tab last opened on crafting menu. Allow crafting quantities of items.

--- gamemode/modules/crafting/cl_crafting.lua
+++ gamemode/modules/crafting/cl_crafting.lua
@@ -26,11 +26,21 @@ net.Receive("craftItem", function()
 		inspect:Remove()
 		inspect = nil
 	end
-	
-	openCrafting(lastButton)
+
+	openCrafting()
 end)
 
-function openCrafting(lastButton)
+local function craftItem(type, index, quantity)
+	local quantity = quantity or 1
+
+	net.Start("craftItem")
+		net.WriteInt(type, 8)
+		net.WriteInt(index, 16)
+		net.WriteInt(quantity, 16)
+	net.SendToServer()
+end
+
+function openCrafting()
 	if frame then
 		frame:Remove()
 		frame = nil
@@ -42,7 +52,7 @@ function openCrafting(lastButton)
 	local buttons = {}
 	local openRecipe, openInfo
 	local recipeFrame, infoFrame
-	
+
 	gui.EnableScreenClicker(true)
 
 	frame = vgui.Create("FalloutRP_Menu")
@@ -50,7 +60,8 @@ function openCrafting(lastButton)
 	frame:SetPos(ScrW()/2 - frame:GetWide()/2, ScrH()/2 - frame:GetTall()/2)
 	frame:SetFontTitle("FalloutRP3", "CRAFTING")
 	frame:AddCloseButton()
-	
+	frame:MakePopup()
+
 	// Draw the buttons at the top for all the item types
 	local offsetX = 0
 	for i = 1, 5 do
@@ -63,18 +74,18 @@ function openCrafting(lastButton)
 		itemType.Paint = function(self, w, h)
 			surface.SetDrawColor(Color(0, 0, 0, 0))
 			surface.DrawRect(0, 0, w, h)
-			
+
 			// The button is highlighted
 			if self.hovered then
 				surface.SetDrawColor(COLOR_AMBER)
 				surface.DrawOutlinedRect(0, 0, w, h)
 			end
-			
+
 			// The button is selected
 			if self.selected and (currentSelectedButton == self) then
 				surface.SetDrawColor(COLOR_AMBERFADE)
 				surface.DrawRect(0, 0, w, h)
-			
+
 				surface.SetDrawColor(COLOR_AMBER)
 				surface.DrawOutlinedRect(0, 0, w, h)
 			end
@@ -91,29 +102,36 @@ function openCrafting(lastButton)
 			self.selected = true
 			self:SetTextColor(COLOR_BLUE)
 			surface.PlaySound("pepboy/click1.wav")
-			
+
 			if currentSelectedButton then
 				// Reset the old button to not be selected
 				currentSelectedButton.selected = false
 				currentSelectedButton:SetTextColor(COLOR_AMBER)
 			end
-			
+
 			currentSelectedButton = self
-			
+
 			openRecipe(i)
+
+			lastButton = i
 		end
-		
+
 		// So we can call the last button we were on when reopening the menu
 		table.insert(buttons, itemType)
-		
+
 		offsetX = offsetX + buttonW + buttonPadding
 	end
-	
+
 	openRecipe = function(type)
 		if recipeFrame then
 			recipeFrame:Remove()
+			recipeFrame = nil
 		end
-		
+		if infoFrame then
+			infoFrame:Remove()
+			infoFrame = nil
+		end
+
 		recipeFrame = vgui.Create("DPanel", frame)
 		recipeFrame:SetPos(buttonPadding, 125)
 		recipeFrame:SetSize(childW, childH)
@@ -121,15 +139,15 @@ function openCrafting(lastButton)
 			surface.SetDrawColor(COLOR_AMBER)
 			surface.DrawOutlinedRect(0, 0, w, h)
 		end
-		
+
 		local scroll = vgui.Create("DScrollPanel", recipeFrame)
 		scroll:SetPos(0, 0)
 		scroll:SetSize(recipeFrame:GetWide(), recipeFrame:GetTall())
 		scroll.Paint = function(self, w, h)
 			surface.SetDrawColor(Color(0, 0, 0, 0))
 			surface.DrawRect(0, 0, w, h)
-		end	
-		
+		end
+
 		local scroller = scroll:GetVBar()
 		scroller.Paint = function(self, w, h)
 			surface.SetDrawColor(Color(0, 0, 0, 0))
@@ -142,39 +160,39 @@ function openCrafting(lastButton)
 		scroller.btnUp.Paint = function(self, w, h)
 			surface.SetDrawColor(Color(0, 0, 0, 0))
 			surface.DrawRect(0, 0, w, h)
-		end	
+		end
 		scroller.btnGrip.Paint = function(self, w, h)
 			surface.SetDrawColor(COLOR_AMBER)
 			surface.SetMaterial(matLineDashed)
 			surface.DrawTexturedRect(0, 0, 3, h)
 		end
-		
+
 		local scrollerW = scroller:GetWide()
 		local layout = vgui.Create("DListLayout", scroll)
 		layout:SetSize(scroll:GetWide() - 10, scroll:GetTall() - 10)
 		layout:SetPos(5, 5)
-		
+
 		local currentItem
 		for k,v in ipairs(RECIPES[type]) do
 			if LocalPlayer():hasCraftingLevel(v.level) then
-			
+
 				local itemBox = vgui.Create("DButton")
 				itemBox:SetSize(layout:GetWide() - scrollerW, 30)
 				itemBox.Paint = function(self, w, h)
 					surface.SetDrawColor(Color(0, 0, 0, 0))
 					surface.DrawRect(0, 0, w, h)
-					
-					
+
+
 					if self.selected and (currentItem == self) then
 						surface.SetDrawColor(COLOR_AMBERFADE)
 						surface.DrawRect(0, 0, w - scrollerW - textPadding*2, h)
-					
+
 						surface.SetDrawColor(COLOR_AMBER)
 						surface.DrawOutlinedRect(0, 0, w - scrollerW - textPadding*2, h)
 					elseif self.hovered then
 						surface.SetDrawColor(Color(255, 182, 66, 30))
 						surface.DrawRect(0, 0, w - scrollerW - textPadding*2, h)
-				
+
 						surface.SetDrawColor(COLOR_AMBER)
 						surface.DrawOutlinedRect(0, 0, w - scrollerW - textPadding*2, h)
 					end
@@ -183,18 +201,18 @@ function openCrafting(lastButton)
 					self.selected = true
 					self:SetTextColor(COLOR_BLUE)
 					surface.PlaySound("pepboy/click1.wav")
-					
+
 					if currentItem then
 						// Reset the old button to not be selected
 						currentItem.selected = false
 					end
-					
+
 					currentItem = self
-					
+
 					openInfo(v, type, k)
 				end
 				itemBox:SetText("")
-				
+
 				local itemLabel = vgui.Create("DLabel", itemBox)
 				itemLabel:SetPos(textPadding, textPadding/2)
 				itemLabel:SetFont("FalloutRP2")
@@ -204,7 +222,7 @@ function openCrafting(lastButton)
 				end
 				itemLabel:SizeToContents()
 				itemLabel:SetTextColor(COLOR_AMBER)
-				
+
 				itemBox.OnCursorEntered = function(self)
 					self.hovered = true
 					surface.PlaySound("pepboy/click2.wav")
@@ -212,32 +230,32 @@ function openCrafting(lastButton)
 				end
 				itemBox.OnCursorExited = function(self)
 					self.hovered = false
-					
+
 					itemLabel:SetTextColor(COLOR_AMBER)
 				end
-				
+
 				layout:Add(itemBox)
 			end
 		end
 	end
-	
-	// Open the last menu when reopening
-	if lastButton then
-		buttons[lastButton].hovered = false
-		buttons[lastButton].selected = true
-		currentSelectedButton = buttons[lastButton]
+
+	// Open the last tab when reopening
+	if lastButton and buttons[lastButton] then
+		buttons[lastButton]:DoClick()
+
 		openRecipe(lastButton)
 	end
-	
+
 	openInfo = function(itemInfo, type, index)
 		if infoFrame then
 			infoFrame:Remove()
+			infoFrame = nil
 		end
-		
+
 		local id = itemInfo.classid
 		local level = itemInfo.level
 		local materials = itemInfo.materials
-		
+
 		infoFrame = vgui.Create("DPanel", frame)
 		infoFrame:SetPos(buttonPadding + childW + (frameW - (childW*2 + buttonPadding*2)), 125)
 		infoFrame:SetSize(childW, childH)
@@ -245,7 +263,7 @@ function openCrafting(lastButton)
 			surface.SetDrawColor(COLOR_AMBER)
 			surface.DrawOutlinedRect(0, 0, w, h)
 		end
-		
+
 		local icon = vgui.Create("SpawnIcon", infoFrame)
 		icon:SetSize(128, 128)
 		icon:SetPos(infoFrame:GetWide()/2 - icon:GetWide()/2, 25)
@@ -266,28 +284,49 @@ function openCrafting(lastButton)
 				inspect = nil
 			end
 		end
-		
+
 		local required = vgui.Create("DLabel", infoFrame)
 		required:SetFont("FalloutRP2")
 		required:SetTextColor(COLOR_AMBER)
 		required:SetText("Requires:")
 		required:SetPos(10, 155)
 		required:SizeToContents()
-		
+
 		local offsetY = 0
-		
+
 		local craft = vgui.Create("FalloutRP_Button", infoFrame)
 		craft:SetPos(infoFrame:GetWide()/2 - craft:GetWide()/2, infoFrame:GetTall() - craft:GetTall() * 1.5)
 		craft:SetText("Craft")
 		craft.DoClick = function(self)
 			if !self:GetDisabled() then
-				net.Start("craftItem")
-					net.WriteInt(type, 8)
-					net.WriteInt(index, 16)
-				net.SendToServer()
+				craftItem(type, index)
 			end
 		end
-		
+
+		local craftX
+		if isStackable(itemInfo.classid) then
+			craft:SetPos(infoFrame:GetWide()/2 - craft:GetWide()*1.5, infoFrame:GetTall() - craft:GetTall() * 1.5)
+
+			craftX = vgui.Create("FalloutRP_Button", infoFrame)
+			craftX:SetPos(infoFrame:GetWide()/2 + craftX:GetWide()/2, infoFrame:GetTall() - craft:GetTall() * 1.5)
+			craftX:SetText("Craft (x)")
+			craftX.DoClick = function(self)
+				if !self:GetDisabled() then
+					local slider = vgui.Create("FalloutRP_NumberWang", frame)
+					slider:SetPos(frame:GetWide()/2 - slider:GetWide()/2, frame:GetTall()/2 - slider:GetTall()/2)
+					slider:SetMinimum(1)
+					slider:SetMaximum(1000)
+					slider:SetValue(1)
+					slider:SetText("Craft")
+					slider:GetButton().DoClick = function()
+						if slider:ValidInput() then
+							craftItem(type, index, slider:GetAmount())
+						end
+					end
+				end
+			end
+		end
+
 		for id, amount in pairs(materials) do
 			local materialName = vgui.Create("DLabel", infoFrame)
 			materialName:SetFont("FalloutRP1")
@@ -295,24 +334,27 @@ function openCrafting(lastButton)
 			materialName:SetText(getItemName(id))
 			materialName:SetPos(10, 185 + offsetY)
 			materialName:SizeToContents()
-			
+
 			local owned, uniqueid = LocalPlayer():getInventoryCount(id)
-			
+
 			local materialOwned = vgui.Create("DLabel", infoFrame)
 			materialOwned:SetFont("FalloutRP1")
 			materialOwned:SetTextColor(COLOR_RED)
 			materialOwned:SetText(owned .. " / " ..amount)
 			materialOwned:SetPos(materialName.x + materialName:GetWide() + 10, 185 + offsetY)
 			materialOwned:SizeToContents()
-			
+
 			// The player has enough of the required item
 			if owned >= amount then
 				materialOwned:SetTextColor(COLOR_LIGHTGREEN)
 			else
 				// The player does not have enough of the required item
-				craft:SetDisabled(true)
+				craft:SetDisabled()
+				if craftX then
+					craftX:SetDisabled()
+				end
 			end
-			
+
 			offsetY = offsetY + 25
 		end
 	end
