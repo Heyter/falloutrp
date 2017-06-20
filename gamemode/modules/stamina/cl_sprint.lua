b2b61f66e582d97c30c33db1dccb8c645d8ebb9b
NickSchleicher <nschle3@students.towson.edu>
2017-06-20 19:29:58 -0400

    Move sprint hud to bottom of screen.

--- gamemode/modules/stamina/cl_sprint.lua
+++ gamemode/modules/stamina/cl_sprint.lua
@@ -2,23 +2,24 @@
 local disableLevel = DISABLE_RUN
 local staminaDrawColor = Color(255, 0, 0, 255)
 
-local w, h = 200, 25
-
 function staminaDraw()
 	if plyDataExists(LocalPlayer()) then
+		local w, h = ScrW(), ScreenScale(10)
+
+		local hudColor = getHUDColor
 		local color = util.getPepboyColor()
 		local stamina = LocalPlayer():GetNWInt("tcb_Stamina")
 		local maxStamina = LocalPlayer():getMaxSprintLength()
 
 		if stamina < maxStamina then
 			surface.SetDrawColor(COLOR_BLACKFADE)
-			surface.DrawRect(ScrW()/2 - w/2, ScrH() - 25, w, h)
+			surface.DrawRect(ScrW()/2 - w/2, ScrH() - h, w, h + 1)
 
-			surface.SetDrawColor(color)
-			surface.DrawRect(ScrW()/2 - w/2, ScrH() - 25, (stamina / maxStamina) * w, h)
+			surface.SetDrawColor(Color(color.r, color.g, color.b, 150))
+			surface.DrawRect(ScrW()/2 - w/2, ScrH() - h, (stamina / maxStamina) * w, h + 1)
 
-			//surface.SetDrawColor(COLOR_AMBER)
-			//surface.DrawOutlinedRect(ScrW()/2 - w/2, ScrH() - 100, w, h)
+			surface.SetDrawColor(Color(hudColor.x + 40, hudColor.y + 40, hudColor.z + 40, 255))
+			surface.DrawOutlinedRect(ScrW()/2 - w/2, ScrH() - h, (stamina / maxStamina) * w, h + 1)
 		end
 	end
 end
