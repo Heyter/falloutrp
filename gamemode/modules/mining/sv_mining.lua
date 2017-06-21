d194dd02218b2bfcc12652a6440e05f12cdb45f8
NickSchleicher <nschle3@students.towson.edu>
2017-06-20 22:43:41 -0400

    Change spawn positions for chests, veins, scavengables.

--- gamemode/modules/mining/sv_mining.lua
+++ gamemode/modules/mining/sv_mining.lua
@@ -9,27 +9,27 @@ end
 function spawnVein(inactiveVeins, type)
 	local randomLocation = table.Random(inactiveVeins)
 	local location = VEINS[type]["Positions"][randomLocation]
-	
+
 	local sizeInfo, size = getRandomSize(type)
 	local prop = table.Random(sizeInfo.Props)
 	local oreCount = math.random(sizeInfo.Amount[1], sizeInfo.Amount[2])
-	
+
 	local vein = ents.Create("vein")
 	vein:SetModel(prop)
 	vein:SetPos(location["Position"])
 	vein:setType(type)
 	vein:setOres(oreCount)
-	
+
 	VEINS[type]["Positions"][randomLocation]["Active"] = true // Set the location to active
 	vein.key = randomLocation // So we know which location to set inactive when the vein is gone
-	
+
 	vein:Spawn()
 end
 
 function getActiveVeins(type)
 	local active = 0
 	local inactive = {}
-	
+
 	for k, v in pairs(VEINS[type]["Positions"]) do
 		if v.Active then
 			active = active + 1
@@ -37,7 +37,7 @@ function getActiveVeins(type)
 			table.insert(inactive, k)
 		end
 	end
-	
+
 	return active, inactive
 end
 
@@ -45,25 +45,21 @@ function getVeinLimit(type)
 	return VEINS[type]["Limit"]
 end
 
-function addVein()
-	local type = "Normal"
-	local roll = util.roll(VEIN_RARE_CHANCE, 100)
-	
-	if roll then
-		type = "Rare"
-	end
+local types = {"Common", "Uncommon", "Rare"}
+function addVeins()
+	for k,v in pairs(types) do
+		local numActive, inactiveVeins = getActiveVeins(v)
 
-	local numActive, inactiveVeins = getActiveVeins(type)
-		
-	if numActive < getVeinLimit(type) then
-		// If we haven't hit the limit for amount of chests on the map
-		spawnVein(inactiveVeins, type)
+		if numActive < getVeinLimit(v) then
+			// If we haven't hit the limit for amount of chests on the map
+			spawnVein(inactiveVeins, v)
+		end
 	end
 end
 
 function beginVeinTimer()
 	timer.Create("veinSpawn", VEIN_TIMER, 0, function()
-		addVein()
+		addVeins()
 	end)
 end
 
@@ -71,4 +67,4 @@ hook.Add("InitPostEntity", "startVeinTimer", function()
 	timer.Simple(5, function()
 		beginVeinTimer()
 	end)
-end)	
+end)
